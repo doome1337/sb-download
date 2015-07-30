a864e430f0feff60c685a3ba2b03434aeb7e34ae
doome1337 <dmitry.paramonov@gmail.com>
2015-07-30 19:41:46 -0400

    Trim is now a method, to prepare for a single file.

--- trimmer.py
+++ trimmer.py
@@ -1,26 +1,22 @@
 import sys
 # 6 Arguments: Post Number, Source File, Target File,
 # Previous File (Blank if none), Index File, Next File (Blank if none)
-postnum = sys.argv[1]
-source = sys.argv[2]
-target = sys.argv[3]
-prevf = sys.argv[4]
-indexf = sys.argv[5]
-nextf = sys.argv[6]
-filein = file(source,"r")
-fileout = file(target,"w")
-fileout.write('<a href='+prevf+'>&lt;&lt;</a>  ')
-fileout.write('<a href='+indexf+'>Index</a>  ')
-fileout.write('<a href='+nextf+'>&gt;&gt;</a><br><br>\n')
-curstring = ""
-while not curstring.find('<li id="post-'+str(postnum))!=-1:
-    curstring = filein.next()
-while not curstring.find("<article>")!=-1:
-    curstring = filein.next()
-while not curstring.find("</article>")!=-1:
-    curstring = filein.next()
-    if not curstring.find("</article>")!=-1:
-        fileout.write(curstring)
-fileout.write('\n<br>\n<a href='+prevf+'>&lt;&lt;</a>  <a href='+indexf+'>Index</a>  <a href='+nextf+'>&gt;&gt;</a>')
-filein.close()
-fileout.close()
+def trim(postnum,source,target,prevf,indexf,nextf):
+    filein = file(source,"r")
+    fileout = file(target,"w")
+    fileout.write('<a href='+prevf+'>&lt;&lt;</a>  ')
+    fileout.write('<a href='+indexf+'>Index</a>  ')
+    fileout.write('<a href='+nextf+'>&gt;&gt;</a><br><br>\n')
+    curstring = ""
+    while not curstring.find('<li id="post-'+str(postnum))!=-1:
+        curstring = filein.next()
+    while not curstring.find("<article>")!=-1:
+        curstring = filein.next()
+    while not curstring.find("</article>")!=-1:
+        curstring = filein.next()
+        if not curstring.find("</article>")!=-1:
+            fileout.write(curstring)
+    fileout.write('\n<br>\n<a href='+prevf+'>&lt;&lt;</a>  <a href='+indexf+'>Index</a>  <a href='+nextf+'>&gt;&gt;</a>')
+    filein.close()
+    fileout.close()
+trim(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
